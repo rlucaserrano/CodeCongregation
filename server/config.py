958fec1886330f11fdd16cedab537686c81655b9
Matthew Golan <mttgln@ssrb-vpn2-9-63.vpn.ufl.edu>
2024-09-26 20:28:50 -0400

    Created database.py and config.py. Connected database and created functions to insert, delete, and modify entries in tables. Created routes for testing.

---
+++ server/config.py
@@ -0,0 +1,23 @@
+
+import os
+from dotenv import load_dotenv
+
+# Educational sources used to setup config.py 
+# 1. https://docs.python.org/3.11/distutils/configfile.html
+# 2. https://www.geeksforgeeks.org/python-os-getenv-method/
+
+load_dotenv()
+
+class Config:
+    
+    # Database
+    ORACLE_USER = os.getenv("ORACLE_USER")
+    ORACLE_PASSWORD = os.getenv("ORACLE_PASSWORD")
+    ORACLE_HOST = os.getenv("ORACLE_HOST")
+    ORACLE_PORT = os.getenv("ORACLE_PORT")
+    ORACLE_SID = os.getenv("ORACLE_SID")
+
+    # Google 
+    GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
+    GOOGLE_SECRET_KEY = os.getenv("GOOGLE_SECRET_KEY")
+
