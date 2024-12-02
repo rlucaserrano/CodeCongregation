0ff46183c6a472c1a83a8e7d41bbb8cddeba8f9b
rlucaserrano <r.serranoarreola@ufl.edu>
2024-12-02 13:58:24 -0500

    modified:   server/main.py 	modified:   wsgi.py

--- server/main.py
+++ server/main.py
@@ -820,4 +820,4 @@ def set_cors_headers(response):
     return response
     
 if __name__ == "__main__":
-    app.run(debug=True, port=8080)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
